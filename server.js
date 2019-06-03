8d709c1e43f718f08d625692993c16661462aba9
Kayla <6lackmacaroon@gmail.com>
2019-06-03 14:08:52 -0600

    local host is running - GET request built

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const db = require('./data/accounts-model.js')
 
 const server = express();
 
 // your code here
+server.get('/', (req, res) => {
+      
+      db.find()
+      .then(accounts => {
+            res.status(200).json(accounts);
+      })
+      .catch(err => {
+            res.status(500).json({ error: 'could not get accounts from database'});
+      })
+});
+
+
 
 module.exports = server;
